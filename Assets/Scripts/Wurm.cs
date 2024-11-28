1cf156da39ed70b31c1327950dc50292f55c7c76
Florian Jakubowski <florian.jakubowski@t-online.de>
2024-11-28 16:55:04 +0100

    Created Wurm Prefab

--- Assets/Scripts/Wurm.cs
+++ Assets/Scripts/Wurm.cs
@@ -4,11 +4,10 @@ using UnityEngine.InputSystem;
 
 public class Wurm : MonoBehaviour
 {
+    //[SerializeField] private Transform wurmTransform;
     [SerializeField] private InputActionAsset controls;
- //   PlayerInput action = new PlayerInput();
- //   private InputAction interact;
-    public Spline spline;
-    public SplineExtrude splineExtrude;
+    [SerializeField] private Spline spline;
+    [SerializeField] private SplineExtrude splineExtrude;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,30 +16,51 @@ public class Wurm : MonoBehaviour
         spline = GetComponent<SplineContainer>().Spline;
         
         Generate(default);
-        var actionMap = controls.FindActionMap("Debug");
-        var inputAction = actionMap.FindAction("Generate");
-        inputAction.performed += Generate;
+        var debugActionMap = controls.FindActionMap("Debug");
+        var generateInputAction = debugActionMap.FindAction("Generate");
+        generateInputAction.performed += Generate;
         
- //       interact = InputSystem.actions.FindAction("Interact");
+        /*var playerActionMap = controls.FindActionMap("Player");
+        var playerInputAction = playerActionMap.FindAction("MoveObject");
+        playerInputAction.performed += MoveObject;*/
     }
 
     public void Generate(InputAction.CallbackContext context)
     {
-        OnInteract();
-        
+        spline.Clear();
+        SetSplineNodes();
+        SetRadius();
+        //SetPosition();
+    }
+
+    private void SetPosition()
+    {
+        //wurmTransform = transform.position(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
+    }
+
+    private void SetSplineNodes()
+    {
+        for (int i = 0; i < Random.Range(3, 10); i++)
+            spline.Add(new Vector3(Random.Range(-5, 5), Random.Range(0, 5), Random.Range(-5, 5)));
+    }
+
+    private void SetRadius()
+    {
+        splineExtrude.Radius = Random.Range(0.1f, 1.0f);
+        splineExtrude.Rebuild();
+    }
+
+    private void MoveObject(InputAction.CallbackContext context)
+    {
+        transform.position = context.ReadValue<Vector3>();
     }
 
     // Update is called once per frame
     void Update()
     {
- //       if (interact.IsPressed())
- //           OnInteract();
     }
 
     void OnInteract()
     {
-        spline.Clear();
-        for (int i = 0; i < Random.Range(3, 10); i++)
-            spline.Add(new Vector3(Random.Range(-5, 5), Random.Range(0, 5), Random.Range(-5, 5)));
     }
 }
